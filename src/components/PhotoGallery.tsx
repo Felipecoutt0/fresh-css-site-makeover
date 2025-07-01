32e3bab1439fd59b3b15118a6beeace6610bbeaa
Felipe Couto <couttofelipe498@gmail.com>
2025-06-30 23:34:05 -0300

    Update PhotoGallery.tsx

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,19 +1,17 @@
+import React from "react";
 
 const PhotoGallery = () => {
   const photos = [
-      { src: '/foto1.jpg', alt: 'Foto juntos' },
-      { src: '/foto2.jpg', alt: 'Outra foto' },
-      { src: '/foto3.jpg', alt: 'Mais uma' },
-      { src: '/foto4.jpg', alt: 'Foto treino' },
-      { src: '/foto6.jpg', alt: 'Foto pizzaria' }
-];
-
+    { src: '/foto1.jpg', alt: 'Foto juntos' },
+    { src: '/foto2.jpg', alt: 'Outra foto' },
+    { src: '/foto3.jpg', alt: 'Mais uma' },
+    { src: '/foto4.jpg', alt: 'Foto treino' },
+    { src: '/foto6.jpg', alt: 'Foto pizzaria' }
+  ];
 
   return (
     <section className="flex justify-center gap-6 p-8 flex-wrap">
-      {photos.map((photo, idx) => (
-        <img key={idx} src={photo.src} alt={photo.alt} />
-          ))} => (
+      {photos.map((photo, index) => (
         <div
           key={index}
           className="w-40 h-40 bg-white p-2 shadow-lg transform -rotate-2 hover:rotate-1 hover:scale-105 transition-all duration-300 rounded-lg border-8 border-white hover:shadow-xl"
